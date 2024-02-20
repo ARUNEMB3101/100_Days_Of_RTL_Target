b40e5ebf0fe5b5e91a0663b1eefc7fa72c37176b
Arun V <127391793+ARUNEMB3101@users.noreply.github.com>
2024-02-20 18:23:19 +0530

    Update Day_28_Mealy_Model_Overlapping_&_Non-Overlapping _001_Sequence_detector.v

--- Day_28_Mealy_Model_Overlapping_&_Non-Overlapping _001_Sequence_detector.v
+++ Day_28_Mealy_Model_Overlapping_&_Non-Overlapping _001_Sequence_detector.v
@@ -1,3 +1,4 @@
+//////////////////////////////////design block of non overlapping sequence detector 1001//////////////////////
 module NO_SD_1001(
    input in,
 	input clck,
@@ -64,4 +65,72 @@ case (prst_st)
 	 end
 endcase
 endmodule
+//////////////////////////////////Design of overlapping sequence detector 1001///////////////////////////
+module Overlap_SD_1001(
+   input in,
+	input clck,
+	input rst,
+	output reg out);
+reg [1:0]prst_st, nxt_st;
+localparam A = 2'b00,
+           B = 2'b01,
+			  C = 2'b10,
+			  D = 2'b11;
+initial 
+begin
+prst_st = 2'b00;
+nxt_st = 2'b00;
+end
+always @(posedge clck, posedge rst)
+begin
+if (rst)
+prst_st = A;
+else 
+prst_st = nxt_st;
+end
+always @(prst_st , in)
+case (prst_st)
+ A: if(in == 0)
+    begin
+	 out = 0;
+	 nxt_st = A;
+	 end
+	 else if (in == 1)
+	 begin
+	 out = 0;
+	 nxt_st = B;
+	 end
+ B: if(in == 0)
+    begin
+	 out = 0;
+	 nxt_st = C;
+	 end
+	 else if (in == 1)
+	 begin
+	 out = 0;
+	 nxt_st = B;
+	 end
+ C: if(in == 0)
+    begin
+	 out = 0;
+	 nxt_st = D;
+	 end
+	 else if (in == 1)
+	 begin
+	 out = 0;
+	 nxt_st = B;
+	 end
+ D: if(in == 0)
+    begin
+	 out = 0;
+	 nxt_st = A;
+	 end
+	 else if (in == 1)
+	 begin
+	 out = 1;
+	 nxt_st = B;
+	 end
+endcase
+endmodule
+
 
